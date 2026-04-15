9cf9d1fb9b33e13d0ce59c4e552426d6ceed5eb5
Tushar Agrawal <agarwaltushar1373@gmail.com>
2026-04-15 23:41:04 +0530

    fix ssl connection

--- app/db/session.py
+++ app/db/session.py
@@ -1,7 +1,12 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from app.core.config import settings
+import ssl
 
-engine = create_async_engine(settings.DATABASE_URL, echo=True, pool_pre_ping=True, pool_size=5, max_overflow=10, connect_args={"ssl": True})
+ssl_context = ssl.create_default_context()
+ssl_context.check_hostname = False
+ssl_context.verify_mode = ssl.CERT_NONE
+
+engine = create_async_engine(settings.DATABASE_URL, echo=True, pool_pre_ping=True, pool_size=5, max_overflow=10, connect_args={"ssl": ssl_context})
 
 SessionLocal = async_sessionmaker(
     bind=engine,
